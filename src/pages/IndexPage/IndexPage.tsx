7842617aba06ae5aa6298225e29d5c19442b7884
sege72kim <sega72kim@mail.ru>
2024-05-08 23:59:43 +0500

    Deleting useless files

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -1,17 +1,7 @@
 import type { FC } from "react"
 import { Page } from "~/components/Page/Page.tsx"
 import "./IndexPage.css" // Assuming your CSS file
-import { Header } from "~/components/Tasks/Header"
-import { InfoBlock } from "~/components/Tasks/InfoBlock"
-import { ButtonInfo } from "~/components/Tasks/ButtonInfo"
-export const IndexPage: FC = () => {
 
-  return (
-    <Page title="Not meme. Group access">
-      <Header/>
-      <InfoBlock/>
-      <ButtonInfo/>
-      
-    </Page>
-  )
+export const IndexPage: FC = () => {
+  return <Page title="Not meme. Group access">=222</Page>
 }
